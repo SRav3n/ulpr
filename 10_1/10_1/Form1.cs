a90c6ba01252943e2e0757ab172b2de6a220b645
Svetlana P <167322150+SRav3n@users.noreply.github.com>
2024-04-19 17:32:35 +0700

    Add files via upload

---
+++ 10_1/10_1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(textBox1.Text);
+                double b = double.Parse(textBox2.Text);
+                double c = double.Parse(textBox3.Text);
+                double S = 2 * (a * c + b * c + b * a);
+                textBox4.Text = S.ToString();
+            }
+            catch (Exception) 
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                MessageBox.Show("бебебебе");
+            }
+        }
+    }
+}
---
+++ 10_2/10_2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = int.Parse(textBox2.Text);
+                int result = n * n;
+                textBox3.Text = result.ToString();
+            }
+            catch(Exception)
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                MessageBox.Show("бебебе");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int p = int.Parse(textBox1.Text);
+            int Result(int n)
+            {
+                int sum = 0;
+                for (int i = 1; i <= 2*n-1; i+=2)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+
+            int result = Result(p);
+            textBox3.Text = result.ToString();
+        }
+    }
+}
